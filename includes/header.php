75082c0660ad65e7566f6fc29f9493b7d98b0e1d
Methew Guda <methewguda@gmail.com>
2016-02-07 14:26:34 -0400

    Added header to ContactForm

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>
+	<?php
+	  if (isset($page_title)): ?>
+	<?php echo $page_title; ?> | <?php endif; ?>Contact
+	</title>
+
+	<!--Include project root for Server Side Include-->
+	<base href=""/> 
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet"
+	    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+	    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" 
+	    crossorigin="anonymous">
+
+	<!-- Custom styles for Template -->
+	<link href="../css/style.css" rel="stylesheet">
+
+	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+	<script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>

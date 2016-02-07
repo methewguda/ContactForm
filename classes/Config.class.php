2252b8e3f6ae4d9f213f0eff3252733d8f2b0802
Methew Guda <methewguda@gmail.com>
2016-02-07 14:17:41 -0400

    Added configure class to ContactForm

---
+++ classes/Config.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Configuration class
+ */
+
+ class Config
+ {
+
+   const SMTP_HOST = ,				//Enter you SMTP Host here 
+         SMTP_PORT = ,				//Enter you SMTP Port here
+         SMTP_CERTIFICATE = ,		//Enter you SMTP Certificate here
+         SMTP_USER = ,				//Enter you SMTP Username here
+         SMTP_PASS = ,				//Enter you SMTP Password here
+         SMTP_NAME = ;				////Enter a name here or you can leave it blank
+ }
